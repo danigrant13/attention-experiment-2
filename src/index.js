8ecdabb973febf113b472905a4b92301b9ed512b
danigrant13 <magr1950@colorado.edu>
2019-08-04 08:49:08 -0600

    Use `MemoryRouter` in production

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, MemoryRouter } from "react-router-dom";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const Router = (process.env.NODE_ENV === 'development' ? BrowserRouter : MemoryRouter)
+
 ReactDOM.render(
-  <BrowserRouter>
+  <Router>
     <App />
-  </BrowserRouter>,
+  </Router>,
   document.getElementById('root')
 );
 
